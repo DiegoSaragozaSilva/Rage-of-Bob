b854775b099e928805a2abb2930c0c95fb00c8a2
unknown <diegosaragozaa2@gmail.com>
2022-11-25 09:37:29 -0300

    Fire rate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed;
+    public float fireRate;
     public GameObject bulletPrefab;
 
+    private float canShoot;
     private Animator animator;
 
     void Start() {
         speed = 5.0f;
+        fireRate = 0.4f;
 
+        canShoot = -1.0f;
         animator = GetComponent<Animator>();
     }
 
@@ -41,22 +45,26 @@ public class Player : MonoBehaviour {
     }
 
     private void combatRoutine() {
-        if (Input.GetKey("up")) {
+        if (Input.GetKey("up") && Time.time > canShoot) {
+            canShoot = Time.time + fireRate;
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, 90));
             bullet.GetComponent<Bullet>().flyDirection = new Vector3(0, 1, 0);
         }
 
-        if (Input.GetKey("right")) {
+        if (Input.GetKey("right") && Time.time > canShoot) {
+            canShoot = Time.time + fireRate;
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, 0));
             bullet.GetComponent<Bullet>().flyDirection = new Vector3(1, 0, 0);
         }
 
-        if (Input.GetKey("down")) {
+        if (Input.GetKey("down") && Time.time > canShoot) {
+            canShoot = Time.time + fireRate;
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, -90));
             bullet.GetComponent<Bullet>().flyDirection = new Vector3(0, -1, 0);
         }
 
-        if (Input.GetKey("left")) {
+        if (Input.GetKey("left") && Time.time > canShoot) {
+            canShoot = Time.time + fireRate;
             GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0, 0, 180));
             bullet.GetComponent<Bullet>().flyDirection = new Vector3(-1, 0, 0);
         }
